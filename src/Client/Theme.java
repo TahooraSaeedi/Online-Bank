99711f82fdc5c3e49e9d467820293cc447d6d627
MohammadRezaRasi <mhmdrzrasi@gmail.com>
2021-07-05 16:30:38 +0430

    Some changes in appearance and linking favorite accounts with app buttons

--- src/Client/Theme.java
+++ src/Client/Theme.java
@@ -54,4 +54,46 @@ public abstract class Theme {
             }
         }
     }
+
+    public static void changeTheme() {
+        switch (theme) {
+            case 1: {
+                button1 = "#A8DADC";
+                button2 = "#E63946";
+                back1 = "#457B9D";
+                back2 = "#A8DADC";
+                back3 = "#F1FAEE";
+                text1 = "#F1FAEE";
+                text2 = "#1D3557";
+                text3 = "#A8DADC";
+                field = "#F1FAEE";
+                break;
+            }
+            case 2: {
+                button1 = "#E5E5E5";
+                button2 = "#1E325C";
+                back1 = "#E5E5E5";
+                back2 = "#000000";
+                back3 = "#E5E5E5";
+                text1 = "#000000";
+                text2 = "#14213D";
+                text3 = "#D6D6D6";
+                field = "#FFFFFF";
+                break;
+            }
+            case 3: {
+                button1 = "#F7EDE2";
+                button2 = "#84A59D";
+                back1 = "#F7EDE2";
+                back2 = "#F6BD60";
+                back3 = "#F7EDE2";
+                text1 = "#F28482";
+                text2 = "#F28482";
+                text3 = "#EDD7BF";
+                field = "#F7EDE2";
+                break;
+            }
+        }
+    }
+
 }
